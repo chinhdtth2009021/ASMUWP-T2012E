ab67e1e42d566ec2cdacf12a5e3f1174eb5d28f1
chinhdtth2009021 <chinhdtth2009021@fpt.edu.vn>
2022-02-16 17:47:58 +0700

    asm-t2012e'

---
+++ assigmentUWP1/Entity/LoginInformation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assigmentUWP1.Entity
+{
+   public class LoginInformation
+    {
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
---
+++ assigmentUWP1/Service/FileService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace assigmentUWP1.Service
+{
+    public class FileService
+    {
+
+        private static string CloudName = "xuanhung2401";
+        private static string CloudApiKey = "882796476526336";
+        private static string CloudApiSecret = "gOOT_72AMyn9TQz1Hd4MxyGRjxY";
+
+        static CloudinaryDotNet.Account account;
+        static CloudinaryDotNet.Cloudinary cloudinary;
+        public FileService()
+        {
+            if (account == null)
+            {
+                account = new CloudinaryDotNet.Account
+                {
+                    Cloud = CloudName,
+                    ApiKey = CloudApiKey,
+                    ApiSecret = CloudApiSecret
+                };
+            }
+            if (cloudinary == null)
+            {
+                cloudinary = new CloudinaryDotNet.Cloudinary(account);
+                cloudinary.Api.Secure = true;
+            }
+        }
+        public async Task<string> UploadFile(StorageFile file)
+        {
+            if (file != null)
+            {
+                CloudinaryDotNet.Actions.RawUploadParams imageUploadParams = new CloudinaryDotNet.Actions.RawUploadParams
+                {
+                    File = new CloudinaryDotNet.FileDescription(file.Name, await file.OpenStreamForReadAsync())
+                };
+                //Thực hiện upload lấy thông tin về.
+                CloudinaryDotNet.Actions.RawUploadResult result = await cloudinary.UploadAsync(imageUploadParams);
+                return result.SecureUrl.OriginalString;
+            }
+            return null;
+        }
+    }
+}
